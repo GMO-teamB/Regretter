63ec1f8484d185377ce868f7499700b7e23a535f
irohasorange <k11200750@yahoo.co.jp>
2022-08-03 23:21:19 +0900

    あああ

--- frontend/src/Components/Loading.js
+++ frontend/src/Components/Loading.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {  useEffect,  useMemo,  useRef, useState } from 'react'
 
 import "./Loading.css";
-import Timer from "react-timer-wrapper";
-import Timecode from "react-timecode";
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default function Loading() {
+  const [seconds, setSeconds]=useState(900);
+  const [isTraining, setIsTraining] = useState(false);
+  const intervalRef = useRef(null);
+  useEffect(()=>{
+    if(isTraining){
+      intervalRef.current = setInterval(() => {
+        console.log(1)
+          setSeconds(seconds => seconds - 1);
+      }, 1000);
+    }else{
+      if(intervalRef.current){
+        clearTimeout(intervalRef.current)
+        intervalRef.current = null
+      }
+    }
+  },[isTraining]);
+  useEffect(()=>{
+    if (seconds === 0 && intervalRef.current !== null){
+      clearInterval(intervalRef.current)
+      intervalRef.current = null
+    }
+  })
+  const transformSec = useMemo(()=>{
+    const min = Math.floor(seconds/60)
+    const sec = seconds - (min * 60)
+    if(sec<10){
+      return `${min}:0${sec}`
+    }
+    return `${min}:${sec}`
+  },[seconds])
   return (
     <div className="container">
       <div className="main">
@@ -17,11 +45,10 @@ export default function Loading() {
         />
         <h2 className="inProgress">現在運動中</h2>
         <div className="time">
-          <Timer active duration={null}>
-            <Timecode />
-          </Timer>
+          {transformSec}
         </div>
-        <Button className="done-btn" component={Link} to="/">終了する</Button>
+        {isTraining?<Button onClick={()=>{setIsTraining(false)}}>一時停止</Button>:<Button onClick={()=>{setIsTraining(true)}}>運動開始</Button>}
+        <Button onClick={()=>{setIsTraining(false);clearTimeout(intervalRef)}}className="done-btn" component={Link} to="/">終了する</Button>
       </div>
     </div>
   );
--- frontend/src/Components/pichart.js
+++ frontend/src/Components/pichart.js
@@ -1,5 +1,5 @@
 import "./pichart.css";
-import { Box, CircularProgress, Grid, Typography, Button } from '@mui/material'
+import { Box, CircularProgress, Grid, Typography, Button, CardMedia } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 
 
@@ -26,14 +26,16 @@ export function PieChart(props){
       </Box>
       <Box position="relative" display="inline-flex" className="circleBox">
         {/* 背景用のCircularProgress */}
-        <CircularProgress className="circle" variant="determinate" size={400} value={100} style={{'color': 'gray'}}/>
+        <CircularProgress className="circle" variant="determinate" size={300} value={100} style={{'color': 'gray'}}/>
         {/* バロメーター用のCircularProgress */}
-        <CircularProgress className="circle" variant="determinate" size={400} value={value} style={{'color': '#14b4c9'}}/>
+        <CircularProgress className="circle" variant="determinate" size={300} value={value} style={{'color': '#14b4c9'}}/>
         <div className="circleContent">
-          <Grid container justify="center" display={"block"}>
-               <Typography>残り</Typography>
+          <Grid container display={"flex"}>
+            <Box display={"block"} >
+              <Typography>残り</Typography>
               <Typography variant="h5" fontSize={60}>{target * mode - now}</Typography>
               <Typography textAlign={"right"}>Kcal</Typography>
+            </Box>
           </Grid>
         </div>
       </Box>
