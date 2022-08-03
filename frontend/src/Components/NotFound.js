534ae81631f036bd1bee5412cf90758be483ba5e
zyusuke <96976665+zyusuke10@users.noreply.github.com>
2022-08-03 16:15:01 +0900

    NotFoundPageの作成

--- frontend/src/Components/NotFound.js
+++ frontend/src/Components/NotFound.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./NotFound.css";
+import {NavLink} from "react-router-dom";
 
 function NotFound() {
   return (
-    <div>Page Not Found !!!</div>
-  )
+    <div className="notFound-container">
+      <div className="notFound-card-wrapper">
+        <div className="notFound-error-msg-wrapper">
+          <h1 className="notFound-error-num">404</h1>
+          <h2 className="notFound-error">エラー！！</h2>
+          <p className="notFound-error-msg">お探しのページが見つかりません</p>
+          <NavLink to="/"  className="notFound-top-button">Topへ</NavLink>
+        </div>
+
+        <div className="notFound-image-container">
+          <img
+            className="notFound-image"
+            src="https://img.freepik.com/free-vector/app-development-illustration_52683-47931.jpg"
+            alt="error-img"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
