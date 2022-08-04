52117c1fa6496c7b5733c58eef90611a137b52de
Mariotcamp <marioqwertyuiop3@gmail.com>
2022-08-05 00:07:17 +0900

    remove-sdk

--- db/schema.rb
+++ db/schema.rb
@@ -31,10 +31,10 @@ ActiveRecord::Schema.define(version: 2022_08_04_065838) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
     t.string "provider"
-    t.string "name"
     t.string "sex"
     t.string "weight"
     t.string "age"
+    t.string "name"
     t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
