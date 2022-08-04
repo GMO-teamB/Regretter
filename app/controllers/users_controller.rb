a287d30210c0c267fd5238f6ee2720f8591638e6
irohasorange <k11200750@yahoo.co.jp>
2022-08-04 14:28:53 +0900

    team33 仮APIの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
-  def show
+  before_action :authenticate_user! 
+  def index
+    user = User.find(3)
+    if user.exist?
+    render json: {
+      user: user.map{user.id | user.sex| user.weight| user.age}
+    }, status: :ok
+    else
+    render json: {}, status::no_content
   end
+
+  def create
+    set_users(@user)
+    if @data.save
+        render json: {
+          user: @data
+        }, status: :created
+    else
+      render json: {}, status: :internal_server_error
+    end    
+  end
+  def set_users(user)
+    @data = user
+    @data.attributes={
+        sex: params[:sex],
+        weight: params[:weight]
+        age: params[:age]
+  }
+  end
+  
+  
+  def update
+
+    # 最終的にredirect_to '/'にする
+  end
+
 end
